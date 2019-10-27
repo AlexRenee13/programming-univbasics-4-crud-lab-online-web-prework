2aa376f44fc2331f35a0b23a7b35397ab2a4189e
Alexis Palacios <alexis1994662@yahoo.com>
2019-10-27 07:28:46 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,39 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  [1,2,3,4]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  add_element_to_end_of_array << (command)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  add_element_to_start_of_array.unshift()
 end
 
 def remove_element_from_end_of_array(array)
-  
+  remove_element_from_end_of_array.pop()
 end
 
 def remove_element_from_start_of_array(array)
-  
+  remove_element_from_start_of_array.shift()
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  retrieve_element_from_index[]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  retrieve_first_element_from_array[]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  retrieve_last_element_from_array[]
 end
 
 def update_element_from_index(array, index_number, element)
-
+update_element_from_index[]=  
 end
