329a48614c6d06cc17991f907ff86dbcd233fb23
Alexis Palacios <alexis1994662@yahoo.com>
2019-10-27 07:58:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,21 +23,17 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  retrieve_element_from_index[1]
-  p retrieve_element_from_index
+  retrieve_element_from_index[2]
 end
 
 def retrieve_first_element_from_array(array)
   retrieve_first_element_from_array[0]
-  p retrieve_first_element_from_array
 end
 
 def retrieve_last_element_from_array(array)
   retrieve_last_element_from_array[-1]
-  p retrieve_last_element_from_array
 end
 
 def update_element_from_index(array, index_number, element)
-update_element_from_index[2]= "chocolate"
-p update_element_from_index
+update_element_from_index[4]= "totally"
 end
