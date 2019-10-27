09cd480175ac290f73aa2dbb8af64c344590c4f8
Alexis Palacios <alexis1994662@yahoo.com>
2019-10-27 07:56:56 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,17 +8,14 @@ end
 
 def add_element_to_end_of_array(array, element)
   add_element_to_end_of_array << "array!"
-  p add_element_to_end_of_array
 end
 
 def add_element_to_start_of_array(array, element)
   add_element_to_start_of_array.unshift("wow")
-
 end
 
 def remove_element_from_end_of_array(array)
-  ya_ya="ya"
-  ya_ya = remove_element_from_end_of_array.pop
+ arrays! = remove_element_from_end_of_array.pop
 end
 
 def remove_element_from_start_of_array(array)
